65ede16e230528517c121e0ffeffd26b83133bbb
David-Worboys <101665132+David-Worboys@users.noreply.github.com>
2024-05-11 23:23:11 +1000

    Fixed imports to reflect new directory structure

--- examples/example_03_all_elements.py
+++ examples/example_03_all_elements.py
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Needed now that library files are in src
 import sys
 
-sys.path.insert(0, "../src")
+sys.path.insert(0, "../src/QTPYGUI")
 
-from src import popups, qtpygui as qtg
+import popups, qtpygui as qtg
 
 
 class Example_03:
